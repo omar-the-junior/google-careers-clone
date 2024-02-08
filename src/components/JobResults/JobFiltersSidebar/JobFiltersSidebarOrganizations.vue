ace813164a84b0e83b2b2865276189a3cf317e0d
omar-the-junior <omarmasoudonline@gmail.com>
2024-02-08 17:28:39 +0200

    Add content to sidebar and create collapsible accordion component

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,69 @@
+<template>
+  <collapsible-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="h-8 w-1/2">
+            <input
+              id="VueTube"
+              type="checkbox"
+              class="mr-3"
+            />
+            <label
+              for="VueTube"
+              class="cursor-pointer"
+              >Vue tube</label
+            >
+          </li>
+          <li class="h-8 w-1/2">
+            <input
+              id="Between Vue and Me"
+              type="checkbox"
+              class="mr-3"
+            />
+            <label
+              for="Between Vue and Me"
+              class="cursor-pointer"
+              >Between vue</label
+            >
+          </li>
+          <li class="h-8 w-1/2">
+            <input
+              id="Vue Brute"
+              type="checkbox"
+              class="mr-3"
+            />
+            <label
+              for="Vue Brute"
+              class="cursor-pointer"
+              >Vue Brute</label
+            >
+          </li>
+          <li class="h-8 w-1/2">
+            <input
+              id="Vue and a Half men"
+              type="checkbox"
+              class="mr-3"
+            />
+            <label
+              for="Vue and a Half men"
+              class="cursor-pointer"
+              >Vue and a half men</label
+            >
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script>
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+
+export default {
+  name: 'JobFiltersSidebarOrganizations',
+  components: {
+    CollapsibleAccordion,
+  },
+}
+</script>
---
+++ tests/unit/components/Shared/CollapsibleAccordion.test.js
@@ -0,0 +1,62 @@
+import {
+  render,
+  screen,
+} from '@testing-library/vue'
+
+import userEvent from '@testing-library/user-event'
+
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+
+describe('CollapsibleAccordion', () => {
+  const renderCollapsibleAccordion = async (
+    config = {}
+  ) => {
+    render(CollapsibleAccordion, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+      slots: {
+        default: '<h3>My nested child</h3>',
+      },
+      props: {
+        header: 'My category',
+      },
+      ...config,
+    })
+  }
+
+  it('renders child content', async () => {
+    renderCollapsibleAccordion()
+
+    expect(
+      screen.queryByText('My nested child')
+    ).not.toBeInTheDocument()
+
+    const button = screen.getByRole('button', {
+      name: /my category/i,
+    })
+
+    await userEvent.click(button)
+    expect(
+      screen.getByText('My nested child')
+    ).toBeInTheDocument()
+  })
+
+  describe('when parent does not provide custom child content', () => {
+    it('render default content', async () => {
+      renderCollapsibleAccordion({
+        slots: null,
+      })
+      const button = screen.getByRole('button', {
+        name: /my category/i,
+      })
+      await userEvent.click(button)
+
+      expect(
+        screen.getByText('No content provided')
+      ).toBeInTheDocument()
+    })
+  })
+})
